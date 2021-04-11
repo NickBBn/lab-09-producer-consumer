8ee76d180a3b23061ce38093ff4b5821355cc03e
NickBBn <nickgeo.winner@gmail.com>
2021-04-11 16:45:08 +0300

    https works

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,19 +1,25 @@
 #include <cstdlib>
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/version.hpp>
-#include <fstream>
+//#include <fstream>
 #include <iostream>
 #include <string>
+//#include <boost/asio/ssl/error.hpp>
+//#include <boost/asio/ssl/stream.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/ssl.hpp>
+#include <boost/beast/version.hpp>
+
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/error.hpp>
+#include <boost/asio/ssl/stream.hpp>
 
 #include "gumbo.h"
 
-namespace ssl = boost::asio::ssl;
-
-void parseUri(std::string_view url, std::string& host, std::string& target) {
-  size_t dd_pos = url.find(':');
+void parseUri(std::string_view url,
+              std::string& protocol, std::string& host, std::string& target) {
+  size_t dd_pos = url.find_first_of(':');
+  protocol = url.substr(0, dd_pos);
   size_t path_start = url.find('/', dd_pos + 3);
   if (path_start == std::string::npos) {
     host = url.substr(dd_pos + 3, url.size() - dd_pos - 2);
@@ -24,16 +30,12 @@ void parseUri(std::string_view url, std::string& host, std::string& target) {
   }
 }
 
-std::string downloadPage(std::string_view url) {
+std::string downloadHttpPage(std::string&& host,
+                      std::string&& target) {
   boost::asio::io_context ioc{};
-
 
   boost::asio::ip::tcp::resolver resolver(ioc);
   boost::beast::tcp_stream stream(ioc);
-
-  std::string host{};
-  std::string target{};
-  parseUri(url, host, target);
 
   auto const results = resolver.resolve(host, "80");
 
@@ -54,10 +56,65 @@ std::string downloadPage(std::string_view url) {
   boost::beast::error_code ec;
   stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
 
-  std::cout << res.body();
-
   return res.body();
 }
+
+std::string downloadHttpsPage(std::string&& host,
+                       std::string&& target) {
+  boost::asio::io_context ioc{};
+
+  boost::asio::ssl::context ctx(boost::asio::ssl::context::tls_client);
+  ctx.set_default_verify_paths();
+  ctx.add_verify_path("/etc/ssl/certs/");
+  ctx.set_verify_mode(boost::asio::ssl::verify_peer);
+
+  boost::asio::ip::tcp::resolver resolver(ioc);
+  boost::beast::ssl_stream<boost::beast::tcp_stream> stream(ioc, ctx);
+
+  if (!SSL_set_tlsext_host_name(stream.native_handle(), host.data())) {
+    boost::beast::error_code ec{static_cast<int>(::ERR_get_error()),
+                                boost::asio::error::get_ssl_category()};
+    throw boost::beast::system_error{ec};
+  }
+
+  auto const results = resolver.resolve(host, "443");
+
+  boost::beast::get_lowest_layer(stream).connect(results);
+
+  stream.handshake(boost::asio::ssl::stream_base::client);
+
+  boost::beast::http::request<boost::beast::http::string_body> req{
+      boost::beast::http::verb::get, target, 11};
+  req.set(boost::beast::http::field::host, host);
+  req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+  boost::beast::http::write(stream, req);
+
+  boost::beast::flat_buffer buffer;
+  boost::beast::http::response<boost::beast::http::string_body> res;
+
+  boost::beast::http::read(stream, buffer, res);
+
+  boost::beast::error_code ec;
+  stream.shutdown(ec);
+
+  return res.body();
+
+}
+
+std::string downloadPage(std::string_view url) {
+  std::string protocol{};
+  std::string host{};
+  std::string target{};
+  parseUri(url, protocol, host, target);
+
+  if (protocol == "http")
+    return downloadHttpPage(std::move(host), std::move(target));
+  else if (protocol == "https")
+    return downloadHttpsPage(std::move(host), std::move(target));
+  return "No protocol";
+}
+
 
 static void search_for_links(GumboNode* node) {
   if (node->type != GUMBO_NODE_ELEMENT) {
@@ -80,7 +137,7 @@ int main(int argc, char** argv) {
     std::cout << "Usage: find_links <html filename>.\n";
     exit(EXIT_FAILURE);
   }
-  const char* url = argv[1];
+  std::string_view url = argv[1];
   std::string res = downloadPage(url);
   std::cout << "Wow we got here"<< std::endl;
 
